9e3f769312e44674b4a9c39033c52f9f1a13e15a
Aidan <aidan.heathfield@gmail.com>
2025-09-03 22:57:17 -0400

    Added wall slide and wall jump

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,24 +1,74 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@export var MAX_FALL_SPEED = 500
+
+# Wall stuff
+@export var WALL_FALL_SPEED = 100
+@export var WALL_JUMP_X_SPEED = 500
+var directionAwayFromWall = 0
+var isWallSliding := false
+var isWallJumping := false
 
 func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("Left", "Right")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if velocity.y > MAX_FALL_SPEED:
+			velocity.y = MAX_FALL_SPEED 
 
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# for when they get on wall initially
+	if is_on_wall() and direction and velocity.y > 0:
+		isWallSliding = true
+		directionAwayFromWall = direction * -1
+	elif not isWallJumping:
+		if not is_on_wall():
+			isWallSliding = false
+			directionAwayFromWall = 0
+		if is_on_floor():
+			isWallSliding = false
+			directionAwayFromWall = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
+	# Handling Inputs
+	if not isWallJumping:
+		HandleDirectionalMovement(direction)
+	else:
+		HandleWallJumpBuffer()
+	
+	HandleWallSlide()
+	HandleJump()
+	
+	move_and_slide()
+
+
+func HandleJump() -> void:
+	if Input.is_action_just_pressed("Jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif isWallSliding:
+			velocity.y = JUMP_VELOCITY
+			velocity.x = directionAwayFromWall * WALL_JUMP_X_SPEED
+			isWallSliding = false
+			
+			# This handles jump buffer to achieve a smooth wall jump
+			isWallJumping = true
+			$WallJumpTimer.start()
+
+func HandleDirectionalMovement(direction) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0
 
-	move_and_slide()
+func HandleWallSlide() -> void:
+	if isWallSliding:
+		velocity.y = WALL_FALL_SPEED
+
+func HandleWallJumpBuffer() -> void:
+	velocity.x = directionAwayFromWall * SPEED
+
+func _on_wall_jump_timer_timeout() -> void:
+	isWallJumping = false
+	directionAwayFromWall = 0
