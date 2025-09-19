7c1a4c3a24497a3a0bde968b8f515e75a0915262
Aidan Heathfield <aidan.heathfield@gmail.com>
2025-09-19 15:03:32 -0400

    Hitting goal now sends you to level select

--- Scenes/goal_post.gd
+++ Scenes/goal_post.gd
@@ -6,4 +6,8 @@ signal goal_reached
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		print("player reached goal")
+		var gm = Global.game_manager
+		gm.remove_2d_scene(gm.SceneAction.DELETE)
+		gm.change_gui_scene("res://Scenes/LevelSelect.tscn", gm.SceneAction.HIDE)
+		gm.current_gui_scene.visible = true
 		goal_reached.emit()
