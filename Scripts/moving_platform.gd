179cd9a70e32aa16d25ad24f2dfbe8d793489bce
MuizHamzat <muizhamzat@gmail.com>
2025-09-25 09:48:14 -0400

    Gave the the platform three parameters: two points to move between, and the speed of the platform

--- Scripts/moving_platform.gd
+++ Scripts/moving_platform.gd
@@ -1,11 +1,20 @@
 extends StaticBody2D
 
+@export var point_a: Vector2 = Vector2.ZERO
+@export var point_b: Vector2 = Vector2.ZERO
+@export var speed: float = 2.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	global_position = point_a
+	move_to_b()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func move_to_b():
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", point_b, speed).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.finished.connect(move_to_a)
+	
+func move_to_a():
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", point_a, speed).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.finished.connect(move_to_b)
