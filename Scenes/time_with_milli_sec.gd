8348d06116dcce5fe2c092fccd531c955a65366a
johnso9 <146889665+johnso9@users.noreply.github.com>
2025-10-21 13:42:22 -0400

    Added new timer type that tracks and displays milliseconds as well. Also changed the viewport size to 1920x1080 (close #8)

---
+++ Scenes/time_with_milli_sec.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@onready var second_timer = $Timer
+@onready var label: Label = $Panel/MarginContainer/Label
+
+var current_time: float = 0.0
+
+
+func _process(delta: float) -> void:
+	current_time += delta
+	label.text = convert_time_to_string(current_time)
+
+
+static func convert_time_to_string(time: float) -> String:
+	var hours: int = int(time / (60.0 * 60.0))
+	var minutes: int = int(time / 60.0) % 60
+	var seconds: int = int(time) % 60
+	var miliseconds: int = int(time * 1000.0) % 1000
+	var string: String = "%02d.%03d" % [seconds, miliseconds]
+	if minutes > 0 or hours > 0:
+		string = string.insert(0, ("%02d:" if hours > 0 else "%02d:") % minutes)
+	if hours > 0:
+		string = string.insert(0, "%d:" % hours)
+	return string
